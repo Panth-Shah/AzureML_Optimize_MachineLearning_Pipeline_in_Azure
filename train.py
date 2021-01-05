0e7025fe3925f0eecc7cd17696afe8dde88dc304
Panth-Shah <panth5shah@gmail.com>
2021-01-05 02:05:33 +0000

    [Udacity Project 1] - Latest changes updated to train.py

--- train.py
+++ train.py
@@ -70,6 +70,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model,'outputs/project_model.joblib')
 
 if __name__ == '__main__':
     main()
